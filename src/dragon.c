stahta01/xroar

commit 1b4c8419987492f06cf1eebe99746e8808b4ccad

contain sound interface in a struct
Lots of knock-on effects:

Sound interface now a publicly exposed element of ao interface.

Sound interface needs to be attached to carts that use it.

src/dragon.c
@@ -49,6 +49,7 @@ Dragon & CoCo 1/2 machine.
 #include "xroar.h"
 
 static struct machine *dragon_new(struct machine_config *mc, struct vo_interface *vo,
+				  struct sound_interface *snd,
 				  struct tape_interface *ti);
 
 struct machine_module machine_dragon_module = {
@@ -84,6 +85,7 @@ struct machine_dragon {
 
 	struct vo_interface *vo;
 	int frame;  // track frameskip
+	struct sound_interface *snd;
 
 	unsigned int ram_size;
 	uint8_t ram[0x10000];
@@ -323,6 +325,7 @@ static void pia1b_data_postwrite(void *sptr);
 static void pia1b_control_postwrite(void *sptr);
 
 static struct machine *dragon_new(struct machine_config *mc, struct vo_interface *vo,
+				  struct sound_interface *snd,
 				  struct tape_interface *ti) {
 	if (!mc)
 		return NULL;
@@ -357,6 +360,7 @@ static struct machine *dragon_new(struct machine_config *mc, struct vo_interface
 	m->op_rts = dragon_op_rts;
 
 	md->vo = vo;
+	md->snd = snd;
 
 	switch (mc->architecture) {
 	case ARCH_DRAGON32:
@@ -432,7 +436,7 @@ static struct machine *dragon_new(struct machine_config *mc, struct vo_interface
 	md->PIA1->b.control_postwrite = DELEGATE_AS0(void, pia1b_control_postwrite, md);
 
 	// Single-bit sound feedback
-	sound_sbs_feedback = DELEGATE_AS1(void, bool, single_bit_feedback, md);
+	md->snd->sbs_feedback = DELEGATE_AS1(void, bool, single_bit_feedback, md);
 
 	// VDG
 	md->VDG0 = mc6847_new(mc->vdg_type == VDG_6847T1);
@@ -1367,7 +1371,7 @@ static void update_sound_mux_source(void *sptr) {
 	struct machine_dragon *md = sptr;
 	unsigned source = ((md->PIA0->b.control_register & (1<<3)) >> 2)
 	                  | ((md->PIA0->a.control_register & (1<<3)) >> 3);
-	sound_set_mux_source(source);
+	sound_set_mux_source(md->snd, source);
 }
 
 static void update_vdg_mode(struct machine_dragon *md) {
@@ -1400,7 +1404,7 @@ static void pia0b_data_preread_coco64k(void *sptr) {
 
 static void pia1a_data_postwrite(void *sptr) {
 	struct machine_dragon *md = sptr;
-	sound_set_dac_level((float)(PIA_VALUE_A(md->PIA1) & 0xfc) / 252.);
+	sound_set_dac_level(md->snd, (float)(PIA_VALUE_A(md->PIA1) & 0xfc) / 252.);
 	tape_update_output(md->tape_interface, md->PIA1->a.out_sink & 0xfc);
 	if (md->is_dragon) {
 		keyboard_update(md);
@@ -1448,14 +1452,14 @@ static void pia1b_data_postwrite(void *sptr) {
 	// Single-bit sound
 	_Bool sbs_enabled = !((md->PIA1->b.out_source ^ md->PIA1->b.out_sink) & (1<<1));
 	_Bool sbs_level = md->PIA1->b.out_source & md->PIA1->b.out_sink & (1<<1);
-	sound_set_sbs(sbs_enabled, sbs_level);
+	sound_set_sbs(md->snd, sbs_enabled, sbs_level);
 	// VDG mode
 	update_vdg_mode(md);
 }
 
 static void pia1b_control_postwrite(void *sptr) {
 	struct machine_dragon *md = sptr;
-	sound_set_mux_enabled(md->PIA1->b.control_register & 0x08);
+	sound_set_mux_enabled(md->snd, md->PIA1->b.control_register & 0x08);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -1486,12 +1490,12 @@ static void vdg_fs(void *sptr, _Bool level) {
 	mc6821_set_cx1(&md->PIA0->b, level);
 	sam_vdg_fsync(md->SAM0, level);
 	if (level) {
-		sound_update();
 		md->frame--;
 		if (md->frame < 0)
 			md->frame = xroar_frameskip;
-		if (md->frame == 0)
+		if (md->frame == 0) {
 			DELEGATE_CALL0(md->vo->vsync);
+		}
 	}
 }
 
@@ -1529,7 +1533,7 @@ static void single_bit_feedback(void *sptr, _Bool level) {
 
 static void update_audio_from_tape(void *sptr, float value) {
 	struct machine_dragon *md = sptr;
-	sound_set_tape_level(value);
+	sound_set_tape_level(md->snd, value);
 	if (value >= 0.5)
 		md->PIA1->a.in_sink &= ~(1<<0);
 	else

src/alsa/ao_alsa.c
@@ -59,9 +59,9 @@ static void *new(void) {
 	struct ao_alsa_interface *aoalsa = xmalloc(sizeof(*aoalsa));
 	*aoalsa = (struct ao_alsa_interface){0};
 	struct ao_interface *ao = &aoalsa->public;
+	const char *errstr = NULL;
 
 	ao->free = DELEGATE_AS0(void, ao_alsa_free, ao);
-	ao->write_buffer = DELEGATE_AS1(voidp, voidp, ao_alsa_write_buffer, ao);
 
 	const char *device = xroar_cfg.ao_device ? xroar_cfg.ao_device : "default";
 	int err;
@@ -220,22 +220,32 @@ static void *new(void) {
 			sample_nbytes = sizeof(float);
 			break;
 		default:
-			LOG_ERROR("Unhandled audio format.\n");
+			errstr = "Unhandled audio format";
 			goto failed;
 	}
 
 	unsigned buffer_size = aoalsa->fragment_nframes * nchannels * sample_nbytes;
 	aoalsa->audio_buffer = xmalloc(buffer_size);
-	sound_init(aoalsa->audio_buffer, buffer_fmt, rate, nchannels, aoalsa->fragment_nframes);
+	ao->sound_interface = sound_interface_new(aoalsa->audio_buffer, buffer_fmt, rate, nchannels, aoalsa->fragment_nframes);
+	if (!ao->sound_interface) {
+		errstr = "XRoar internal error";
+		goto failed;
+	}
+	ao->sound_interface->write_buffer = DELEGATE_AS1(voidp, voidp, ao_alsa_write_buffer, ao);
 	LOG_DEBUG(1, "\t%u frags * %ld frames/frag = %ld frames buffer (%ldms)\n", nfragments, aoalsa->fragment_nframes, buffer_nframes, (buffer_nframes * 1000) / rate);
 
 	/* snd_pcm_writei(aoalsa->pcm_handle, buffer, aoalsa->fragment_nframes); */
 	return aoalsa;
 
 failed:
-	LOG_ERROR("Failed to initialise ALSA: %s\n", snd_strerror(err));
-	if (aoalsa)
+	if (!errstr)
+		errstr = snd_strerror(err);
+	LOG_ERROR("Failed to initialise ALSA: %s\n", errstr);
+	if (aoalsa) {
+		if (aoalsa->audio_buffer)
+			free(aoalsa->audio_buffer);
 		free(aoalsa);
+	}
 	return NULL;
 }
 
@@ -244,6 +254,7 @@ static void ao_alsa_free(void *sptr) {
 
 	snd_pcm_close(aoalsa->pcm_handle);
 	snd_config_update_free_global();
+	sound_interface_free(aoalsa->public.sound_interface);
 	free(aoalsa->audio_buffer);
 	free(aoalsa);
 }

src/ao.h
@@ -20,11 +20,11 @@ Audio output interface definition.
 #include "delegate.h"
 
 struct module;
+struct sound_interface;
 
 struct ao_interface {
 	DELEGATE_T0(void) free;
-
-	DELEGATE_T1(voidp, voidp) write_buffer;
+	struct sound_interface *sound_interface;
 };
 
 extern struct module * const *ao_module_list;

src/jack/ao_jack.c
@@ -22,11 +22,15 @@
 
 #include "config.h"
 
+// for struct timespec, gettimeofday
+#define _POSIX_C_SOURCE 200112L
+
 #include <errno.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/time.h>
 
@@ -78,7 +82,6 @@ static void *new(void) {
 	struct ao_interface *ao = &aojack->public;
 
 	ao->free = DELEGATE_AS0(void, ao_jack_free, ao);
-	ao->write_buffer = DELEGATE_AS1(voidp, voidp, ao_jack_write_buffer, ao);
 
 	const char **ports;
 
@@ -133,14 +136,20 @@ static void *new(void) {
 
 	aojack->fragment_buffer = NULL;
 
-	sound_init(aojack->fragment_buffer, sample_fmt, rate, 1, fragment_nframes);
+	ao->sound_interface = sound_interface_new(aojack->fragment_buffer, sample_fmt, rate, 1, fragment_nframes);
+	if (!ao->sound_interface) {
+		LOG_ERROR("Failed to initialise JACK: XRoar internal error\n");
+		goto failed;
+	}
+	ao->sound_interface->write_buffer = DELEGATE_AS1(voidp, voidp, ao_jack_write_buffer, ao);
 	LOG_DEBUG(1, "\t%u frags * %u frames/frag = %u frames buffer (%.1fms)\n", aojack->nfragments, fragment_nframes, buffer_nframes, (float)(buffer_nframes * 1000) / rate);
 
 	return aojack;
 
 failed:
-	if (aojack)
+	if (aojack) {
 		free(aojack);
+	}
 	return NULL;
 }
 
@@ -161,6 +170,7 @@ static void ao_jack_free(void *sptr) {
 
 	pthread_cond_destroy(&aojack->fragment_cv);
 	pthread_mutex_destroy(&aojack->fragment_mutex);
+	sound_interface_free(aojack->public.sound_interface);
 	free(aojack);
 }
 

src/machine.c
@@ -226,9 +226,8 @@ void machine_shutdown(void) {
 	machine_modules = NULL;
 }
 
-struct machine *machine_dragon_new(struct machine_config *mc, struct vo_interface *vo);
-
-struct machine *machine_new(struct machine_config *mc, struct vo_interface *vo, struct tape_interface *ti) {
+struct machine *machine_new(struct machine_config *mc, struct vo_interface *vo,
+			    struct sound_interface *snd, struct tape_interface *ti) {
 	if (!mc) {
 		return NULL;
 	}
@@ -246,7 +245,7 @@ struct machine *machine_new(struct machine_config *mc, struct vo_interface *vo, 
 				LOG_DEBUG(2, "Machine module: %s\n", req_type);
 				LOG_DEBUG(1, "Machine: %s\n", mc->description);
 			}
-			m = mm->new(mc, vo, ti);
+			m = mm->new(mc, vo, snd, ti);
 			break;
 		}
 	}

src/machine.h
@@ -21,8 +21,9 @@ Machine & machine config handling.
 #include "breakpoint.h"
 #include "xconfig.h"
 
-struct slist;
 struct cart;
+struct slist;
+struct sound_interface;
 struct tape_interface;
 struct vo_interface;
 
@@ -228,7 +229,8 @@ struct machine {
 void machine_init(void);
 void machine_shutdown(void);
 
-struct machine *machine_new(struct machine_config *mc, struct vo_interface *vo, struct tape_interface *ti);
+struct machine *machine_new(struct machine_config *mc, struct vo_interface *vo,
+			    struct sound_interface *snd, struct tape_interface *ti);
 
 /* Helper function to populate breakpoints from a list. */
 #define machine_bp_add_list(m, list, sptr) (m)->bp_add_n(m, list, sizeof(list) / sizeof(struct machine_bp), sptr)
@@ -237,7 +239,7 @@ struct machine *machine_new(struct machine_config *mc, struct vo_interface *vo, 
 struct machine_module {
 	const char *name;
 	const char *description;
-	struct machine *(* const new)(struct machine_config *mc, struct vo_interface *vo, struct tape_interface *ti);
+	struct machine *(* const new)(struct machine_config *mc, struct vo_interface *vo, struct sound_interface *snd, struct tape_interface *ti);
 };
 
 int machine_load_rom(const char *path, uint8_t *dest, off_t max_size);

src/macosx/ao_macosx.c
@@ -83,7 +83,6 @@ static void *new(void) {
 	struct ao_interface *ao = &aomacosx->public;
 
 	ao->free = DELEGATE_AS0(void, ao_macosx_free, ao);
-	ao->write_buffer = DELEGATE_AS1(voidp, voidp, ao_macosx_write_buffer, ao);
 
 	AudioObjectPropertyAddress propertyAddress;
 	AudioStreamBasicDescription deviceFormat;
@@ -181,14 +180,28 @@ static void *new(void) {
 		pthread_mutex_unlock(&aomacosx->fragment_mutex);
 	}
 
-	sound_init(aomacosx->fragment_buffer[0], sample_fmt, rate, nchannels, fragment_nframes);
+	ao->sound_interface = sound_interface_new(aomacosx->fragment_buffer[0], sample_fmt, rate, nchannels, fragment_nframes);
+	if (!ao->sound_interface) {
+		LOG_ERROR("Failed to initialise Mac OS X audio: XRoar internal error\n");
+		goto failed;
+	}
+	ao->sound_interface->write_buffer = DELEGATE_AS1(voidp, voidp, ao_macosx_write_buffer, ao);
 	LOG_DEBUG(1, "\t%u frags * %u frames/frag = %u frames buffer (%.1fms)\n", aomacosx->nfragments, fragment_nframes, buffer_nframes, (float)(buffer_nframes * 1000) / rate);
 
 	return aomacosx;
 
 failed:
-	if (aomacosx)
+	if (aomacosx) {
+		if (aomacosx->fragment_buffer) {
+			if (aomacosx->nfragments > 1) {
+				for (unsigned i = 0; i < aomacosx->nfragments; i++) {
+					free(aomacosx->fragment_buffer[i]);
+				}
+			}
+			free(aomacosx->fragment_buffer);
+		}
 		free(aomacosx);
+	}
 	return NULL;
 }
 

src/null/ao_null.c
@@ -68,9 +68,13 @@ static void *new(void) {
 	struct ao_interface *ao = &aonull->public;
 
 	ao->free = DELEGATE_AS0(void, ao_null_free, ao);
-	ao->write_buffer = DELEGATE_AS1(voidp, voidp, ao_null_write_buffer, ao);
 
-	sound_init(NULL, SOUND_FMT_NULL, 44100, 1, 1024);
+	ao->sound_interface = sound_interface_new(NULL, SOUND_FMT_NULL, 44100, 1, 1024);
+	if (!ao->sound_interface) {
+		free(aonull);
+		return NULL;
+	}
+	ao->sound_interface->write_buffer = DELEGATE_AS1(voidp, voidp, ao_null_write_buffer, ao);
 	aonull->last_pause_cycle = event_current_tick;
 	aonull->last_pause_ms = current_time();
 	return aonull;

commit 6f844769f977085e8cf252635e8a62b768271466

update sound before vsync
This was broken in 1b4c8419987492f06cf1eebe99746e8808b4ccad

src/dragon.c
@@ -1490,6 +1490,7 @@ static void vdg_fs(void *sptr, _Bool level) {
 	mc6821_set_cx1(&md->PIA0->b, level);
 	sam_vdg_fsync(md->SAM0, level);
 	if (level) {
+		sound_update(md->snd);
 		md->frame--;
 		if (md->frame < 0)
 			md->frame = xroar_frameskip;