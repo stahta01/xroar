5c047f0fa5153c376b83d9f937a3af5b675f2d5b
Ciaran Anscomb <xroar@6809.org.uk>
2021-10-11 12:07:26 +0100

    orch90: serialisation

--- src/orch90.c
+++ src/orch90.c
@@ -20,13 +20,17 @@
 #include "config.h"
 #endif
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "array.h"
+
 #include "cart.h"
 #include "logging.h"
 #include "part.h"
+#include "serialise.h"
 #include "sound.h"
 
 static struct cart *orch90_new(struct cart_config *);
@@ -39,27 +43,48 @@ struct cart_module cart_orch90_module = {
 
 struct orch90 {
 	struct cart cart;
-	float left;
-	float right;
+	uint8_t left;
+	uint8_t right;
 	struct sound_interface *snd;
 };
+
+static const struct ser_struct ser_struct_orch90[] = {
+	SER_STRUCT_ELEM(struct orch90, cart, ser_type_unhandled), // 1
+	SER_STRUCT_ELEM(struct orch90, left, ser_type_uint8), // 2
+	SER_STRUCT_ELEM(struct orch90, right, ser_type_uint8), // 3
+};
+
+#define N_SER_STRUCT_ORCH90 ARRAY_N_ELEMENTS(ser_struct_orch90)
+
+#define ORCH90_SER_CART  (1)
 
 static uint8_t orch90_write(struct cart *c, uint16_t A, _Bool P2, _Bool R2, uint8_t D);
 static void orch90_reset(struct cart *c);
 static void orch90_attach(struct cart *c);
 static void orch90_detach(struct cart *c);
+static void orch90_serialise(struct part *p, struct ser_handle *sh);
 static _Bool orch90_has_interface(struct cart *c, const char *ifname);
 static void orch90_attach_interface(struct cart *c, const char *ifname, void *intf);
 
-static struct cart *orch90_new(struct cart_config *cc) {
+static _Bool orch90_finish(struct part *p) {
+	struct orch90 *o = (struct orch90 *)p;
+	// Nothing to do...
+	(void)o;
+	return 1;
+}
+
+static struct orch90 *orch90_create(void) {
 	struct orch90 *o = part_new(sizeof(*o));
 	struct cart *c = &o->cart;
 	*o = (struct orch90){0};
 	part_init(&c->part, "orchestra-90");
 	c->part.free = cart_rom_free;
+	c->part.serialise = orch90_serialise;
+	c->part.finish = orch90_finish;
+	c->part.is_a = cart_is_a;
 
-	c->config = cc;
 	cart_rom_init(c);
+
 	c->write = orch90_write;
 	c->reset = orch90_reset;
 	c->attach = orch90_attach;
@@ -67,10 +92,58 @@ static struct cart *orch90_new(struct cart_config *cc) {
 	c->has_interface = orch90_has_interface;
 	c->attach_interface = orch90_attach_interface;
 
-	o->left = 0.0;
-	o->right = 0.0;
+	return o;
+}
+
+static struct cart *orch90_new(struct cart_config *cc) {
+	assert(cc != NULL);
+
+	struct orch90 *o = orch90_create();
+	struct cart *c = &o->cart;
+	struct part *p = &c->part;
+	c->config = cc;
+
+	if (!orch90_finish(p)) {
+		part_free(p);
+		return NULL;
+	}
 
 	return c;
+}
+
+static void orch90_serialise(struct part *p, struct ser_handle *sh) {
+	struct orch90 *o = (struct orch90 *)p;
+	for (int tag = 1; !ser_error(sh) && (tag = ser_write_struct(sh, ser_struct_orch90, N_SER_STRUCT_ORCH90, tag, o)) > 0; tag++) {
+		switch (tag) {
+		case ORCH90_SER_CART:
+			cart_serialise(&o->cart, sh, tag);
+			break;
+		default:
+			ser_set_error(sh, ser_error_format);
+			break;
+		}
+	}
+	ser_write_close_tag(sh);
+}
+
+struct part *orch90_deserialise(struct ser_handle *sh) {
+	struct orch90 *o = orch90_create();
+	int tag;
+	while (!ser_error(sh) && (tag = ser_read_struct(sh, ser_struct_orch90, N_SER_STRUCT_ORCH90, o))) {
+		switch (tag) {
+		case ORCH90_SER_CART:
+			cart_deserialise(&o->cart, sh);
+			break;
+		default:
+			ser_set_error(sh, ser_error_format);
+			break;
+		}
+	}
+	if (ser_error(sh)) {
+		part_free((struct part *)o);
+		return NULL;
+	}
+	return (struct part *)o;
 }
 
 static void orch90_reset(struct cart *c) {
@@ -101,12 +174,12 @@ static uint8_t orch90_write(struct cart *c, uint16_t A, _Bool P2, _Bool R2, uint
 	(void)P2;
 	(void)R2;
 	if (A == 0xff7a) {
-		o->left = (float)D / 255.;
-		sound_set_external_left(o->snd, o->left);
+		o->left = D;
+		sound_set_external_left(o->snd, (float)D / 255.);
 	}
 	if (A == 0xff7b) {
-		o->right = (float)D / 255.;
-		sound_set_external_right(o->snd, o->right);
+		o->right = D;
+		sound_set_external_right(o->snd, (float)D / 255.);
 	}
 	return D;
 }
