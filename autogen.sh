9eabe5b08618710cd51bf73d6938335e37897ad9
Ciaran Anscomb <xroar@6809.org.uk>
2022-02-15 17:52:07 +0000

    update configure.ac, m4/sdl.m4
    for autoconf 2.71 (hopefully 2.69+)

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,4 @@ test -n "$srcdir" && cd "$srcdir"
 
 echo "Updating build configuration files, please wait...."
 
-test -z "$AUTOMAKE" && AUTOMAKE="automake"
-
-AUTOMAKE="$AUTOMAKE --foreign" autoreconf -fi
+autoreconf -fi
