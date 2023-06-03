aedeb3e9b8141e5d2137932347aefd5cd26e5f29
Ciaran Anscomb <xroar@6809.org.uk>
2023-06-03 18:25:16 +0100

    sdl: simplify resizing
    Don't destroy the renderer each time.  The scaling hints are tested when
    creating a texture, so in order to change them it's ok to just destroy
    and recreate said texture.

--- src/sdl2/vo_sdl2.c
+++ src/sdl2/vo_sdl2.c
@@ -67,8 +67,7 @@ struct vo_sdl_interface {
 	SDL_Renderer *sdl_renderer;
 	int filter;
 
-	int window_w;
-	int window_h;
+	struct vo_window_area window_area;
 
 #ifdef WINDOWS32
 	_Bool showing_menu;
@@ -89,10 +88,6 @@ static void resize(void *sptr, unsigned int w, unsigned int h);
 static void draw(void *sptr);
 static int set_fullscreen(void *sptr, _Bool fullscreen);
 static void set_menubar(void *sptr, _Bool show_menubar);
-
-static _Bool create_renderer(struct vo_sdl_interface *vosdl);
-static void destroy_window(void);
-static void destroy_renderer(struct vo_sdl_interface *vosdl);
 
 static void *new(void *sptr) {
 	struct ui_sdl2_interface *uisdl2 = sptr;
@@ -149,8 +144,8 @@ static void *new(void *sptr) {
 	memset(vosdl->texture.pixels, 0, TEX_BUF_WIDTH * 240 * vosdl->texture.pixel_size);
 
 	vosdl->filter = vo_cfg->gl_filter;
-	vosdl->window_w = 640;
-	vosdl->window_h = 480;
+	vosdl->window_area.w = 640;
+	vosdl->window_area.h = 480;
 
 	vo->free = DELEGATE_AS0(void, vo_sdl_free, vo);
 
@@ -170,10 +165,44 @@ static void *new(void *sptr) {
 	SDL_SetWindowMinimumSize(uisdl2->vo_window, 160, 120);
 	uisdl2->vo_window_id = SDL_GetWindowID(uisdl2->vo_window);
 	vo->show_menubar = 1;
-	if (!create_renderer(vosdl)) {
-		vo_sdl_free(vo);
-		return NULL;
+
+	// Create renderer
+	for (unsigned i = 0; i < ARRAY_N_ELEMENTS(renderer_flags); i++) {
+		vosdl->sdl_renderer = SDL_CreateRenderer(global_uisdl2->vo_window, -1, renderer_flags[i]);
+		if (vosdl->sdl_renderer)
+			break;
 	}
+	if (!vosdl->sdl_renderer) {
+		LOG_ERROR("Failed to create renderer\n");
+		return 0;
+	}
+
+	if (logging.level >= 3) {
+		SDL_RendererInfo renderer_info;
+		if (SDL_GetRendererInfo(vosdl->sdl_renderer, &renderer_info) == 0) {
+			LOG_PRINT("SDL_GetRendererInfo()\n");
+			LOG_PRINT("\tname = %s\n", renderer_info.name);
+			LOG_PRINT("\tflags = 0x%x\n", renderer_info.flags);
+			if (renderer_info.flags & SDL_RENDERER_SOFTWARE)
+				LOG_PRINT("\t\tSDL_RENDERER_SOFTWARE\n");
+			if (renderer_info.flags & SDL_RENDERER_ACCELERATED)
+				LOG_PRINT("\t\tSDL_RENDERER_ACCELERATED\n");
+			if (renderer_info.flags & SDL_RENDERER_PRESENTVSYNC)
+				LOG_PRINT("\t\tSDL_RENDERER_PRESENTVSYNC\n");
+			if (renderer_info.flags & SDL_RENDERER_TARGETTEXTURE)
+				LOG_PRINT("\t\tSDL_RENDERER_TARGETTEXTURE\n");
+			for (unsigned i = 0; i < renderer_info.num_texture_formats; i++) {
+				LOG_PRINT("\ttexture_formats[%u] = %s\n", i, SDL_GetPixelFormatName(renderer_info.texture_formats[i]));
+			}
+			LOG_PRINT("\tmax_texture_width = %d\n", renderer_info.max_texture_width);
+			LOG_PRINT("\tmax_texture_height = %d\n", renderer_info.max_texture_height);
+		}
+	}
+
+	// The actual resolution specified here doesn't seem to matter, it's
+	// effectively setting an aspect ratio, meaning SDL determines what the
+	// Picture Area is for us.
+	SDL_RenderSetLogicalSize(vosdl->sdl_renderer, 640, 480);
 
 #ifdef WINDOWS32
 	// Need an event handler to prevent events backing up while menus are
@@ -184,100 +213,23 @@ static void *new(void *sptr) {
 	// Initialise keyboard
 	sdl_os_keyboard_init(global_uisdl2->vo_window);
 
+	// Initial resize (dimensions actually ignored and taken from window)
+	// creates the texture.
+	resize(vosdl, 640, 480);
+
 	return vo;
 }
 
-static void resize(void *sptr, unsigned int w, unsigned int h) {
-	struct vo_sdl_interface *vosdl = sptr;
-	(void)w;
-	(void)h;
-	create_renderer(vosdl);
-}
-
-static int set_fullscreen(void *sptr, _Bool fullscreen) {
-	struct vo_sdl_interface *vosdl = sptr;
-
-#ifdef HAVE_WASM
-	// Until WebAssembly fullscreen interaction becomes a little more
-	// predictable, we just don't support it.
-	return 0;
-#endif
-
-	_Bool is_fullscreen = SDL_GetWindowFlags(global_uisdl2->vo_window) & (SDL_WINDOW_FULLSCREEN|SDL_WINDOW_FULLSCREEN_DESKTOP);
-
-	if (is_fullscreen == fullscreen) {
-		return 0;
-	}
-
-	SDL_SetWindowFullscreen(global_uisdl2->vo_window, fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
-
-	if (!fullscreen) {
-		// Testing under Wine, returning from fullscreen doesn't
-		// _always_ set it back to the original geometry.  I have no
-		// idea why, so force it:
-		SDL_SetWindowSize(global_uisdl2->vo_window, vosdl->window_w, vosdl->window_h);
-	}
-
-	return 0;
-}
-
-static void set_menubar(void *sptr, _Bool show_menubar) {
+static void resize(void *sptr, unsigned int w_ignored, unsigned int h_ignored) {
 	struct vo_sdl_interface *vosdl = sptr;
 	struct vo_interface *vo = &vosdl->public;
+	(void)w_ignored;
+	(void)h_ignored;
 
-	vo->show_menubar = show_menubar;
-#ifdef WINDOWS32
-	if (show_menubar && !vosdl->showing_menu) {
-		sdl_windows32_add_menu(global_uisdl2->vo_window);
-	} else if (!show_menubar && vosdl->showing_menu) {
-		sdl_windows32_remove_menu(global_uisdl2->vo_window);
-	}
-	if (!vo->is_fullscreen) {
-		SDL_SetWindowSize(global_uisdl2->vo_window, vosdl->window_w, vosdl->window_h);
-	}
-#endif
-}
-
-static void destroy_window(void) {
-	if (global_uisdl2->vo_window) {
-		sdl_os_keyboard_free(global_uisdl2->vo_window);
-		SDL_DestroyWindow(global_uisdl2->vo_window);
-		global_uisdl2->vo_window = NULL;
-	}
-}
-
-// Whenever the window size changes, we recreate the renderer and texture.
-
-static _Bool create_renderer(struct vo_sdl_interface *vosdl) {
-	struct vo_interface *vo = &vosdl->public;
-
-	// XXX 2020-02-23
-	//
-	// There currently seems to be a bug in the Emscripten GL support,
-	// manifesting in SDL2:
-	//
-	// https://github.com/emscripten-ports/SDL2/issues/92
-	//
-	// But probably due to a more low-level bug:
-	//
-	// https://github.com/emscripten-core/emscripten/pull/9803
-	//
-	// Until this is fixed, we do NOT destroy the renderer in Wasm builds.
-	// We do recreate the texture though, as that seems to still work and
-	// then the new scale hints are respected.
-	//
-	// Extra bug points: this doesn't actually seem to fix mousemotion
-	// events in Chromium!  Though button presses are getting through.
-
-#ifndef HAVE_WASM
-	// Remove old renderer & texture, if they exist
-	destroy_renderer(vosdl);
-#else
 	if (vosdl->texture.texture) {
 		SDL_DestroyTexture(vosdl->texture.texture);
 		vosdl->texture.texture = NULL;
 	}
-#endif
 
 	int w, h;
 	SDL_GetWindowSize(global_uisdl2->vo_window, &w, &h);
@@ -312,8 +264,8 @@ static _Bool create_renderer(struct vo_sdl_interface *vosdl) {
 			h = 120;
 			resize_again = 1;
 		}
-		vosdl->window_w = w;
-		vosdl->window_h = h;
+		vosdl->window_area.w = w;
+		vosdl->window_area.h = h;
 	}
 
 	if (resize_again) {
@@ -333,59 +285,11 @@ static _Bool create_renderer(struct vo_sdl_interface *vosdl) {
 	SDL_SetHint(SDL_HINT_RENDER_DRIVER, "direct3d11");
 #endif
 
-#ifdef HAVE_WASM
-	// XXX see above
-	if (!vosdl->sdl_renderer) {
-#endif
-
-	for (unsigned i = 0; i < ARRAY_N_ELEMENTS(renderer_flags); i++) {
-		vosdl->sdl_renderer = SDL_CreateRenderer(global_uisdl2->vo_window, -1, renderer_flags[i]);
-		if (vosdl->sdl_renderer)
-			break;
-	}
-	if (!vosdl->sdl_renderer) {
-		LOG_ERROR("Failed to create renderer\n");
-		return 0;
-	}
-
-#ifdef HAVE_WASM
-	}
-#endif
-
-	if (logging.level >= 3) {
-		SDL_RendererInfo renderer_info;
-		if (SDL_GetRendererInfo(vosdl->sdl_renderer, &renderer_info) == 0) {
-			LOG_PRINT("SDL_GetRendererInfo()\n");
-			LOG_PRINT("\tname = %s\n", renderer_info.name);
-			LOG_PRINT("\tflags = 0x%x\n", renderer_info.flags);
-			if (renderer_info.flags & SDL_RENDERER_SOFTWARE)
-				LOG_PRINT("\t\tSDL_RENDERER_SOFTWARE\n");
-			if (renderer_info.flags & SDL_RENDERER_ACCELERATED)
-				LOG_PRINT("\t\tSDL_RENDERER_ACCELERATED\n");
-			if (renderer_info.flags & SDL_RENDERER_PRESENTVSYNC)
-				LOG_PRINT("\t\tSDL_RENDERER_PRESENTVSYNC\n");
-			if (renderer_info.flags & SDL_RENDERER_TARGETTEXTURE)
-				LOG_PRINT("\t\tSDL_RENDERER_TARGETTEXTURE\n");
-			for (unsigned i = 0; i < renderer_info.num_texture_formats; i++) {
-				LOG_PRINT("\ttexture_formats[%u] = %s\n", i, SDL_GetPixelFormatName(renderer_info.texture_formats[i]));
-			}
-			LOG_PRINT("\tmax_texture_width = %d\n", renderer_info.max_texture_width);
-			LOG_PRINT("\tmax_texture_height = %d\n", renderer_info.max_texture_height);
-		}
-	}
-
-#ifdef HAVE_WASM
-	// XXX see above
-	if (!vosdl->texture.texture)
-#endif
 	vosdl->texture.texture = SDL_CreateTexture(vosdl->sdl_renderer, vosdl->texture.format, SDL_TEXTUREACCESS_STREAMING, TEX_BUF_WIDTH, 240);
 	if (!vosdl->texture.texture) {
 		LOG_ERROR("Failed to create texture\n");
-		destroy_renderer(vosdl);
-		return 0;
+		abort();
 	}
-
-	SDL_RenderSetLogicalSize(vosdl->sdl_renderer, 640, 480);
 
 	SDL_RenderClear(vosdl->sdl_renderer);
 	SDL_RenderPresent(vosdl->sdl_renderer);
@@ -393,33 +297,74 @@ static _Bool create_renderer(struct vo_sdl_interface *vosdl) {
 	global_uisdl2->draw_area.x = global_uisdl2->draw_area.y = 0;
 	global_uisdl2->draw_area.w = w;
 	global_uisdl2->draw_area.h = h;
-
-	return 1;
 }
 
-static void destroy_renderer(struct vo_sdl_interface *vosdl) {
-	if (vosdl->texture.texture) {
-		SDL_DestroyTexture(vosdl->texture.texture);
-		vosdl->texture.texture = NULL;
+static int set_fullscreen(void *sptr, _Bool fullscreen) {
+	struct vo_sdl_interface *vosdl = sptr;
+
+#ifdef HAVE_WASM
+	// Until WebAssembly fullscreen interaction becomes a little more
+	// predictable, we just don't support it.
+	return 0;
+#endif
+
+	_Bool is_fullscreen = SDL_GetWindowFlags(global_uisdl2->vo_window) & (SDL_WINDOW_FULLSCREEN|SDL_WINDOW_FULLSCREEN_DESKTOP);
+
+	if (is_fullscreen == fullscreen) {
+		return 0;
 	}
-	if (vosdl->sdl_renderer) {
-		SDL_DestroyRenderer(vosdl->sdl_renderer);
-		vosdl->sdl_renderer = NULL;
+
+	SDL_SetWindowFullscreen(global_uisdl2->vo_window, fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
+
+	if (!fullscreen) {
+		// Testing under Wine, returning from fullscreen doesn't
+		// _always_ set it back to the original geometry.  I have no
+		// idea why, so force it:
+		SDL_SetWindowSize(global_uisdl2->vo_window, vosdl->window_area.w, vosdl->window_area.h);
 	}
+
+	return 0;
+}
+
+static void set_menubar(void *sptr, _Bool show_menubar) {
+	struct vo_sdl_interface *vosdl = sptr;
+	struct vo_interface *vo = &vosdl->public;
+
+	vo->show_menubar = show_menubar;
+#ifdef WINDOWS32
+	if (show_menubar && !vosdl->showing_menu) {
+		sdl_windows32_add_menu(global_uisdl2->vo_window);
+	} else if (!show_menubar && vosdl->showing_menu) {
+		sdl_windows32_remove_menu(global_uisdl2->vo_window);
+	}
+	if (!vo->is_fullscreen) {
+		SDL_SetWindowSize(global_uisdl2->vo_window, vosdl->window_area.w, vosdl->window_area.h);
+	}
+#endif
 }
 
 static void vo_sdl_free(void *sptr) {
 	struct vo_sdl_interface *vosdl = sptr;
+
 	if (vosdl->texture.pixels) {
 		free(vosdl->texture.pixels);
 		vosdl->texture.pixels = NULL;
 	}
-	// XXX even though destroy_renderer() is called every time the window
-	// resizes with no issues, for some reason calling it here (before or
-	// after freeing the texture pixels) causes a SEGV deep down in the
-	// video driver.  So just don't.
-	//destroy_renderer(vosdl);
-	destroy_window();
+
+	// TODO: I used to have a note here that destroying the renderer caused
+	// a SEGV deep down in the video driver.  This doesn't seem to happen
+	// in my current environment, but I need to test it in others.
+	if (vosdl->sdl_renderer) {
+		SDL_DestroyRenderer(vosdl->sdl_renderer);
+		vosdl->sdl_renderer = NULL;
+	}
+
+	if (global_uisdl2->vo_window) {
+		sdl_os_keyboard_free(global_uisdl2->vo_window);
+		SDL_DestroyWindow(global_uisdl2->vo_window);
+		global_uisdl2->vo_window = NULL;
+	}
+
 	free(vosdl);
 }
 
