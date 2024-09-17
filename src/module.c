3e09ae54dc8ba482579a5c3a3932846bbfe467c7
Ciaran Anscomb <xroar@6809.org.uk>
2024-09-17 13:04:51 +0100

    module.c: tweak argument testing, remove old interface check

--- src/module.c
+++ src/module.c
@@ -18,6 +18,7 @@
 
 #include "top-config.h"
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -28,66 +29,69 @@
 #include "module.h"
 
 static void module_print_list(struct module * const *list) {
-	int i;
-	if (list == NULL || list[0]->name == NULL) {
+	if (!list) {
 		puts("\tNone found.");
 		return;
 	}
-	for (i = 0; list[i]; i++) {
+	for (int i = 0; list[i]; i++) {
 		printf("\t%-10s %s\n", list[i]->name, list[i]->description);
 	}
 }
 
 struct module *module_select(struct module * const *list, const char *name) {
-	int i;
-	if (list == NULL)
+	if (!list) {
 		return NULL;
-	for (i = 0; list[i]; i++) {
-		if (strcmp(list[i]->name, name) == 0)
+	}
+	for (int i = 0; list[i]; i++) {
+		if (0 == strcmp(list[i]->name, name)) {
 			return list[i];
+		}
 	}
 	return NULL;
 }
 
 struct module *module_select_by_arg(struct module * const *list, const char *name) {
-	if (name == NULL)
-		return list[0];
+	if (!name) {
+		return list ? list[0] : NULL;
+	}
 	if (0 == c_strcasecmp(name, "help")) {
 		module_print_list(list);
 		exit(EXIT_SUCCESS);
+	}
+	if (!list) {
+		return NULL;
 	}
 	return module_select(list, name);
 }
 
 void *module_init(struct module *module, void *cfg) {
-	if (!module)
+	if (!module) {
 		return NULL;
+	}
 	const char *description = module->description ? module->description : "unknown";
 	LOG_DEBUG(1, "[%s] %s\n", module->name, description);
-	// New interface?
-	if (module->new) {
-		void *m = module->new(cfg);
-		if (!m) {
-			LOG_DEBUG(1, "%s: init failed: %s\n", module->name, description);
-		}
-		return m;
+	assert(module->new != NULL);
+	void *m = module->new(cfg);
+	if (!m) {
+		LOG_DEBUG(1, "%s: init failed: %s\n", module->name, description);
 	}
-	LOG_ERROR("%s: old module interface called\n", module->name);
-	abort();
+	return m;
 }
 
 void *module_init_from_list(struct module * const *list, struct module *module, void *cfg) {
-	int i;
 	/* First attempt to initialise selected module (if given) */
 	void *m = module_init(module, cfg);
-	if (m)
+	if (m) {
 		return m;
-	if (list == NULL)
+	}
+	if (!list) {
 		return NULL;
+	}
 	/* If that fails, try every *other* module in the list */
-	for (i = 0; list[i]; i++) {
-		if (list[i] != module && (m = module_init(list[i], cfg)))
+	for (int i = 0; list[i]; i++) {
+		if (list[i] != module && (m = module_init(list[i], cfg))) {
 			return m;
+		}
 	}
 	return NULL;
 }
