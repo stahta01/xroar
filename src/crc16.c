52a305eabba5ad872a670ec4800e8fade1ad985a
Ciaran Anscomb <xroar@6809.org.uk>
2024-10-28 13:30:40 +0000

    crc16: add non-ccitt crc16
    Public Domain, adapted from SDL code.

--- src/crc16.c
+++ src/crc16.c
@@ -10,6 +10,29 @@
 #include "crc16.h"
 
 /**
+ * CRC-16, polynomial 0x8005 (reversed here).
+ *
+ * Adapted from SDL code which in turn is flagged as:
+ *
+ * Public domain CRC implementation adapted from:
+ * http://home.thep.lu.se/~bjorn/crc/crc32_simple.c
+ */
+
+uint16_t crc16_byte(uint16_t crc, uint8_t value) {
+	for (int i = 0; i < 8; ++i) {
+		crc = (((crc ^ value) & 1) ? 0xa001 : 0) ^ (crc >> 1);
+		value >>= 1;
+	}
+	return crc;
+}
+
+uint16_t crc16_block(uint16_t crc, const uint8_t *block, unsigned length) {
+	for (; length; length--)
+		crc = crc16_byte(crc, *(block++));
+	return crc;
+}
+
+/**
  * CRC-16-CCITT with bytes processed high bit first ("big-endian"), as used in
  * the WD279X FDC (polynomial 0x1021).  In the FDC, CRC is initialised to
  * 0xffff and NOT inverted before appending to the message.
