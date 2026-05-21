1dd8c6ebbe7c6ad3b96046815b7582d0e56443f2
Ciaran Anscomb <xroar@6809.org.uk>
2026-05-21 16:05:01 +0100

    new breakpoint code
    Intent is to remove the nebulous concept of breakpoint "sessions" and
    the debug_cpu abstraction.
    
    Instead lists will be per-machine, and machines will handle the
    hooks, albeit using helper functions still in breakpoint.c.
    
    We can also use the new symbol tables to simplify breakpoint lists, e.g.
    in tape acceleration code.

--- src/breakpoint.c
+++ src/breakpoint.c
@@ -2,7 +2,7 @@
  *
  *  \brief Breakpoint tracking for debugging.
  *
- *  \copyright Copyright 2011-2024 Ciaran Anscomb
+ *  \copyright Copyright 2011-2026 Ciaran Anscomb
  *
  *  \licenseblock This file is part of XRoar, a Dragon/Tandy CoCo emulator.
  *
@@ -18,6 +18,10 @@
 
 #include "top-config.h"
 
+// Comment this out for debugging
+#define BP_DEBUG(...)
+
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,6 +34,96 @@
 #include "logging.h"
 #include "machine.h"
 #include "part.h"
+
+#ifndef BP_DEBUG
+#define BP_DEBUG(...) LOG_DEBUG(__VA_ARGS__)
+#define BP_MOD_DEBUG(l,...) LOG_MOD_DEBUG(l, "breakpoint", __VA_ARGS__)
+#else
+#define BP_MOD_DEBUG(l,...)
+#endif
+
+static int compar_breakpoint_a(const void *aa, const void *bb);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+_Bool bp_breakpoint_add(struct bp_breakpoint_set *bbs, uint32_t A,
+			DELEGATE_T2(void, bool, uint32) handler) {
+	struct bp_breakpoint key = { .A = A };
+	struct bp_breakpoint *ent = bbs->list ? bsearch(&key, bbs->list, bbs->nbreakpoints, sizeof(key), compar_breakpoint_a) : NULL;
+	if (!ent) {
+		++bbs->nbreakpoints;
+		if (bbs->nbreakpoints > bbs->nallocated) {
+			bbs->nallocated = bbs->nbreakpoints;
+			bbs->list = xrealloc(bbs->list, bbs->nallocated * sizeof(*bbs->list));
+		}
+		ent = &bbs->list[bbs->nbreakpoints-1];
+		*ent = (struct bp_breakpoint){0};
+		ent->A = A;
+		ent->handler[0] = handler;
+		qsort(bbs->list, bbs->nbreakpoints, sizeof(*bbs->list), compar_breakpoint_a);
+		bbs->modified = 1;
+		BP_MOD_DEBUG(2, "added new breakpoint @ 0x%04x\n", A);
+		return 1;
+	}
+	for (int i = 0; i < BP_HANDLERS_PER_ADDRESS; ++i) {
+		if (DELEGATE_EQ(ent->handler[i], handler)) {
+			BP_MOD_DEBUG(2, "breakpoint already exists with handler @ 0x%04x\n", A);
+			return 1;
+		}
+	}
+	for (int i = 0; i < BP_HANDLERS_PER_ADDRESS; ++i) {
+		if (!DELEGATE_DEFINED(ent->handler[i])) {
+			BP_MOD_DEBUG(2, "added handler to existing breakpoint @ 0x%04x\n", A);
+			ent->handler[i] = handler;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void bp_breakpoint_remove(struct bp_breakpoint_set *bbs, int32_t A,
+			  DELEGATE_T2(void, bool, uint32) handler) {
+	for (size_t i = 0; i < bbs->nbreakpoints; ) {
+		struct bp_breakpoint *ent = &bbs->list[i];
+		// A < 0 implies match all addresses
+		if (A < 0 || (uint32_t)A == ent->A) {
+			int n = 0;
+			for (int j = 0; j < BP_HANDLERS_PER_ADDRESS; ++j) {
+				// Similarly, NULL handler func/sptr implies match any
+				if (DELEGATE_DEFINED(ent->handler[j])) {
+					if ((!handler.func || handler.func == ent->handler[j].func) &&
+					    (!handler.sptr || handler.sptr == ent->handler[j].sptr)) {
+						ent->handler[j].func = NULL;
+						BP_MOD_DEBUG(2, "removed handler from breakpoint @ 0x%04x\n", ent->A);
+						continue;
+					}
+					n++;
+				}
+			}
+			if (n == 0) {
+				BP_MOD_DEBUG(2, "removed breakpoint @ 0x%04x\n", ent->A);
+				--bbs->nbreakpoints;
+				size_t ncopy = bbs->nbreakpoints - i;
+				if (ncopy > 0) {
+					memmove(ent, ent + 1, ncopy * sizeof(*ent));
+				}
+				bbs->modified = 1;
+				// continue at current index after moving
+				continue;
+			}
+		}
+		++i;
+	}
+	// If there's nothing left, free the list
+	if (bbs->nbreakpoints == 0 && bbs->list) {
+		bbs->nallocated = 0;
+		free(bbs->list);
+		bbs->list = NULL;
+		BP_MOD_DEBUG(2, "breakpoint list empty: freed\n");
+	}
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 struct bp_session_private {
 	struct bp_session bps;
@@ -231,6 +325,38 @@ static void bp_instruction_hook(void *sptr) {
 		old_pc = cur_pc;
 		cur_pc = DELEGATE_CALL(bpsp->debug_cpu->get_pc);
 	} while (old_pc != cur_pc);
+}
+
+static int compar_breakpoint_a(const void *aa, const void *bb) {
+	const struct bp_breakpoint *a = aa;
+	const struct bp_breakpoint *b = bb;
+	if (a->A < b->A)
+		return -1;
+	return (a->A > b->A);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// Instruction hook
+
+void bp_instruction_hook_new(void *sptr, uint32_t A) {
+	struct bp_breakpoint_set *bbs = sptr;
+	struct bp_breakpoint key = { .A = A };
+	struct bp_breakpoint *ent = NULL;
+	bbs->modified = 1;  // initial symbol fetch
+	for (int i = 0; i < BP_HANDLERS_PER_ADDRESS; ++i) {
+		if (bbs->modified) {
+			// subsequent iterations won't refetch the symbol unless
+			// an add/remove operation sets the modified flag
+			bbs->modified = 0;
+			if (!bbs->list)
+				return;
+			ent = bsearch(&key, bbs->list, bbs->nbreakpoints, sizeof(key), compar_breakpoint_a);
+			if (!ent)
+				return;
+		}
+		DELEGATE_SAFE_CALL(ent->handler[i], 1, A);
+	}
 }
 
 #ifdef WANT_GDB_TARGET
--- src/breakpoint.h
+++ src/breakpoint.h
@@ -2,7 +2,7 @@
  *
  *  \brief Breakpoint tracking for debugging.
  *
- *  \copyright Copyright 2011-2024 Ciaran Anscomb
+ *  \copyright Copyright 2011-2026 Ciaran Anscomb
  *
  *  \licenseblock This file is part of XRoar, a Dragon/Tandy CoCo emulator.
  *
@@ -19,9 +19,47 @@
 #ifndef XROAR_BREAKPOINT_H_
 #define XROAR_BREAKPOINT_H_
 
+#include <stdlib.h>
+
 #include "delegate.h"
 
 struct machine;
+struct slist;
+
+#define BP_HANDLERS_PER_ADDRESS (3)
+
+/*
+ * Breakpoints.  Trap on execution from address.
+ */
+
+struct bp_breakpoint {
+	uint32_t A;
+	DELEGATE_T2(void, bool, uint32) handler[BP_HANDLERS_PER_ADDRESS];
+};
+
+struct bp_breakpoint_set {
+	size_t nbreakpoints;
+	size_t nallocated;
+	struct bp_breakpoint *list;
+	_Bool modified;  // an add or delete has occurred during list run
+};
+
+// Add a breakpoint.  Returns 1 if successful, else 0 (e.g. identical
+// breakpoint already exists)
+_Bool bp_breakpoint_add(struct bp_breakpoint_set *bbs, uint32_t A,
+			DELEGATE_T2(void, bool, uint32) handler);
+
+// Remove specific breakpoint.
+void bp_breakpoint_remove(struct bp_breakpoint_set *bbs, int32_t A,
+			  DELEGATE_T2(void, bool, uint32) handler);
+
+// Remove all breakpoints in a list matching supplied delegate data pointer.
+void bp_breakpoint_remove_all(struct bp_breakpoint_set *bbs, void *sptr);
+
+// Dispatch all breakpoint handlers for a given address.
+void bp_instruction_hook_new(void *sptr, uint32_t A);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 /*
  * Breakpoint support both for internal hooks and user-added traps (e.g. via
@@ -84,5 +122,7 @@ void bp_wp_remove(struct bp_session *bps, unsigned type, unsigned addr, unsigned
 
 void bp_wp_read_hook(struct bp_session *bps, unsigned address);
 void bp_wp_write_hook(struct bp_session *bps, unsigned address);
+
+void bp_instruction_hook_new(void *sptr, uint32_t A);
 
 #endif
